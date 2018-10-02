5ac65b828f157f3e69d365eb6443e2798b9c9870
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-02 12:39:53 +0200

    Assignment + stretch goals + styling begun

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,52 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  state = {
+    date: new Date()
+  }
+
+  startClock = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  componentDidMount() {
+    this.startClock()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  handleStopTimeClick = () => {
+    clearInterval(this.timerID)
+  }
+
+  handleStartTimeClick = () => {
+    this.startClock()
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <p class="clock-face">{this.state.date.toLocaleTimeString()}.</p>
+        <button onClick={this.handleStopTimeClick}>Stop</button>
+        <button onClick={this.handleStartTimeClick}>Start</button>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById("root")
+)
