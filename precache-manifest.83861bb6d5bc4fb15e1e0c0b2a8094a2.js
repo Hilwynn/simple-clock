e17d6cf4f9dce9710f93c8d6d92bcbc3b7464984
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-16 17:11:54 +0200

    Updates

---
+++ precache-manifest.83861bb6d5bc4fb15e1e0c0b2a8094a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/simple-clock/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "6a07bba3014148c56aac",
+    "url": "/simple-clock/static/js/runtime~main.6a07bba3.js"
+  },
+  {
+    "revision": "662768e9eb2ccc2b293f",
+    "url": "/simple-clock/static/js/main.662768e9.chunk.js"
+  },
+  {
+    "revision": "f4d10aee93265461729a",
+    "url": "/simple-clock/static/js/1.f4d10aee.chunk.js"
+  },
+  {
+    "revision": "662768e9eb2ccc2b293f",
+    "url": "/simple-clock/static/css/main.b9d1e968.chunk.css"
+  },
+  {
+    "revision": "8c5c718652da834a6d283c0f4343f2a6",
+    "url": "/simple-clock/index.html"
+  }
+];
